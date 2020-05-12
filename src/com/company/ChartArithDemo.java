bb0224d119e31afbc29e566509ecbadfe64a1e49
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-11 20:59:24 -0500

    added math calculation

---
+++ src/com/company/ChartArithDemo.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ChartArithDemo {
+
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++;
+        System.out.println("ch is now " + ch);
+        
+
+    }
+}
