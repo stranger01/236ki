58b17f05611f5f86ff4150a763325ef14eb0a8fe
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-13 21:03:40 -0500

    added the dinamyc init section

---
+++ src/com/company/DynInit.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double radious = 4, height = 5;
+
+        double volume = 3.1416 * radious * radious *height;
+
+        System.out.println("Volume is " + volume);
+    }
+}
