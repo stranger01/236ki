0ae2a67700a9f6607d11dfa1cd0916615acdb16b
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-16 18:20:44 -0500

    java fundaments section 8

--- src/CalcEngine/Main.java
+++ src/CalcEngine/Main.java
@@ -1,32 +1,50 @@
 package CalcEngine;
 
+/*
+ * This is the code for section 7
+ * After -> Language fundamentals
+ */
+
+import javax.sound.midi.SysexMessage;
+
 public class Main {
 
     public static void main(String[] args) {
-
-
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.d , 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d , 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.d, 'm');
-
-
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for(MathEquation equation : equations) {
             equation.execute();
             System.out.print("result = ");
             System.out.println(equation.getResult());
         }
+
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute((double)leftInt, rightInt);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
     }
 
-    public  static MathEquation create (double leftVal, double rightVal, char opCode){
-
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-
-        return equation;
-    }
 }
--- src/CalcEngine/MathEquation.java
+++ src/CalcEngine/MathEquation.java
@@ -1,44 +1,49 @@
 package CalcEngine;
 
 public class MathEquation {
-
-
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
-    public double getLeftVal() {
-        return leftVal;
-    }
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
 
-    public void setLeftVal(double leftVal) {
-        this.leftVal = leftVal;
-    }
+    public double getResult() { return result;}
 
-    public double getRightVal() {
-        return rightVal;
-    }
+    public MathEquation() {}
 
-    public void setRightVal(double rightVal) {
-        this.rightVal = rightVal;
-    }
-
-    public char getOpCode() {
-        return opCode;
-    }
-
-    public void setOpCode(char opCode) {
+    public MathEquation(char opCode) {
         this.opCode = opCode;
     }
 
-    public double getResult() {
-        return result;
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
     }
 
-    public void execute(){
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
 
+        execute();
+    }
 
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+        result = (int)result;
+    }
+
+    public void execute() {
         switch(opCode) {
             case 'a':
                 result = leftVal + rightVal;
@@ -59,8 +64,4 @@ public class MathEquation {
         }
 
     }
-
-
-
-
 }
