b77145d4c6fc58885b689ba251b94dacad64d71b
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-15 21:02:47 -0500

    added the module demo class

---
+++ src/com/company/ModDemo.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ModDemo {
+    public static void main(String[] args) {
+        int iresult, irem;
+        double dresult, drem;
+
+        iresult = 10 / 3;
+        irem = 10 % 3;
+        dresult = 10.0 / 3.0;
+        drem = 10.0 % 30.;
+
+        System.out.println("REsult and reminder of 10/3: " +
+                iresult + "" + irem);
+        System.out.println("Result and remainder of 10.0 / 3.0 " +
+                dresult + "" + drem);
+
+    }
+}
