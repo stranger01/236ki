10c0f841154091a022fe56e5f837c40fb1181043
Stranger01 <joseantoniosysnet@gmail.com>
2020-06-04 19:23:07 -0500

    updated the break statemens

---
+++ src/com/company/BreakDemo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        int num;
+
+        num = 100;
+
+        for (int i = 0; i < num ; i++) {
+            if (i*i >= num) break;
+            System.out.println(i + "");
+        }System.out.println("Loop complete");
+    }
+}
