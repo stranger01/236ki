6f8bca0344f0b117f2719628493fe8442c9ad252
Stranger01 <joseantoniosysnet@gmail.com>
2020-06-11 17:18:10 -0500

    methods added in a new brach

---
+++ src/methods/RethMech.java
@@ -0,0 +1,39 @@
+package methods;
+
+
+class Vehicle {
+    int passengers;
+    int fuelcap;
+    int mpg;
+
+    int range() {
+        return mpg * fuelcap;
+    }
+}
+
+class RethMech {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        int range1, range2;
+
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        range1 = minivan.range();
+        range2 = minivan.range();
+
+        System.out.println("minivan can carry : " + minivan.passengers
+        + " with a range of  " + range1 + " Miles");
+
+        System.out.println("sportscar can carry : " + sportscar.passengers
+                + " with a range of  " + range1 + " Miles");
+
+    }
+}
